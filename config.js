c49bb056241b3694d2d02cb6a676ca7007e8a059
VyHuy2404 <86751596+VyHuy2404@users.noreply.github.com>
2021-07-01 07:24:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    introTitle: 'Bé con à!',
+    introDesc: `Em là 1102, Anh là 2204
+    Vì em vừa là độc nhất vô nhị, vừa là một nửa của anh <3`,
+    btnIntro: 'Dạ',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnYes: 'Vẫn cứ là yêu anh <33',
+    btnNo: 'Không, Anh à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    reply: 'Bởi vì em là bà xã của anh :33333333',
+    mess: 'Anh biết mà 🥰. Yêu vợ rất nhiều 😘😘',
+    messDesc: 'Anh yêu em rất rất nhiều bà xã ạ :>',
+    btnAccept: 'Vânggggg <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
